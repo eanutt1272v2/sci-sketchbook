673716f0166301f0c96a3df0e6cdbc2546d05fab
Edward <eanutt1272.v2@gmail.com>
2026-03-01 18:19:30 +0000

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 function formatBytes(bytes) {
     if (bytes === -1) return '--';
