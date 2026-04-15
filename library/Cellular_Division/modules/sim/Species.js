11278905e4089db58ae8a744685c78ac6bf5d470
eanutt1272.v2 <eanutt1272.v2@gmail.com>
2026-04-15 20:36:20 +0100

    Delete library/Cellular_Division directory

--- library/Cellular_Division/modules/sim/Species.js
+++
@@ -1,8 +0,0 @@
-class Species {
-  constructor(alpha, beta, gamma, radius) {
-    this.alphaRad = radians(alpha);
-    this.betaRad = radians(beta);
-    this.velocity = (radius * gamma) / 100.0;
-    this.radiusSquared = sq(radius);
-  }
-}
